37ccbc2f5e7e0f36b12be580312280e09e0f0f7b
livelife4ursoul <livelife4ursoul@gmail.com>
2022-11-28 12:33:18 -0800

    added array of objects with individual key values

---
+++ js/scipts.js
@@ -0,0 +1,43 @@
+//array of different pokemon objects and individual key values.
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Arbok',
+        height: 3.5,
+        types: ['poison']
+    },
+    {
+        name: 'Pichu',
+        height: 0.3,
+        types: ['electric']
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        types: ['fire']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1,
+        types: ['bug', 'flying']
+    },
+    {
+        name: 'Ninetales',
+        height: 1.1,
+        types: ['fire']
+    },
+    {
+        name: 'Parasect',
+        height: 1,
+        types: ['grass', 'bug']
+    }
+];
